3e5d820d93fe36692e9d140f1b086d6da86d18c8
OmairXcluesiv <omair@xcluesiv.com>
2017-02-06 15:41:18 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -17,11 +17,16 @@ try {
 } catch (Exception $e) {
 }
 
+function ref_tags($data){
+  
+  return strip_tags($data);
+}
+
 // link of config ebay to get the data
 $html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.XAmerican+Revolutionary+War&_nkw=American+Revolutionary+War&_sacat=0");
 
 $count= 0; // Intial state of counter
-$max_loop = 50; // set the loop value 
+$max_loop= 50; // set the loop value 
 
    for($i=0;$i<=$max_loop;$i++){
      $dom = new simple_html_dom();
@@ -34,27 +39,13 @@ $max_loop = 50; // set the loop value
  
  $sno = $i;
  echo "Sno: " . $sno . "\n";
- echo "Product Title: " . strip_tags($r[$i]) . "\n";
+ echo "Product Title: " .  ref_tags($r[$i]) . "\n";
  $p_text = strip_tags($m[$i]); // replace all HTML tags with plain text 
  $no_ws =  preg_replace('/\s+/', '', $p_text); // remove one or more than one whitespace from grabbed data
  echo "Product Price:"  . $no_ws . "\n\n\n\n\n";
  
  $articles = array(array('sno' => $sno , 'title' => strip_tags($r[$i]) , 'price' => $no_ws));
  
- foreach($articles as $article){
-   $exists = $db->query("SELECT * from data WHERE sno=". $db->quote($article->sno))->fetchObject();
- if(!$exists){
-   $sql = "INSERT into data (sno, title, price) VALUES (:sno, :title, :price)";
- }
- else{
-   $sql = "UPDATE data SET title = :title, price = :price WHERE sno = :sno";
- }
- $statement = $db->prepare($sql);
-    $statement->execute(array(
-    ':sno' => $article['sno'], 
-    ':title' => $article['title'],
-    ':price' => $article['price']
-  ));
-  }
  }
 ?>
+
