114644fb5043150b900ea70cc0979ebe66d1c570
OmairXcluesiv <omair@xcluesiv.com>
2017-02-06 17:05:42 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -23,22 +23,31 @@ function data_refine($data){
   return $ws;
 }
 
-// link of config ebay to get the data
-$html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.XAmerican+Revolutionary+War&_nkw=American+Revolutionary+War&_sacat=0");
-
 $count= 0; // Intial state of counter
 $max_loop= 50; // set the loop value (end)
+$loop_end = 300;
 
    for($i=0;$i<=$max_loop;$i++){
+     
+     // link of config ebay to get the data
+     if($count>=51){
+     $html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=American+Revolutionary+War&_pgn=".$ic."&_skc=50&rt=nc");
+     }
+     
      $dom = new simple_html_dom();
      $dom->load($html);
      $r = $dom->find("a.vip");
      $m = $dom->find("li.lvprice");
 
     //To count the collected data
-   if($r[$i]  || $m[$i] !=""){ $count++;}
+   if($r[$i]  || $m[$i] !=""){ 
+    $count++;
+   if($count>$max_loop)
+     $icount = 1;
+     $ic += $icount;
+   }
      
- $sno = rand(5,500);
+ $sno = $i;
  echo "Sno: " . $sno . "\n";
  echo "Product Title: " .  strip_tags($r[$i]) . "\n";
  echo "Product Price: " .  data_refine($m[$i]) . "\n\n\n\n\n";
